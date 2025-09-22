84c06d6260fadada00495e31f947089a6eb2bdb2
cowinsure <cowinsure@gmail.com>
2025-09-22 15:52:12 +0600

    bug: client side component issue

--- app/breeding/page.tsx
+++ app/breeding/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AuthGuard } from "@/components/auth-guard";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
